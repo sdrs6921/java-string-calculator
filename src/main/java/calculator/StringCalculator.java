743f7ac106ebacf66891cd0d3de24268dc96ba85
Beenie93 <seongbeen93@naver.com>
2021-06-15 00:21:08 +0900

    feat: null 또는 빈 문자열일 경우 0을 반환하는 로직 추가

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class StringCalculator {
+
+    public static int splitAndSum(String expression) {
+        int result = 0;
+
+        if (expression == null || expression.isEmpty()) {
+            return result;
+        }
+
+        return result;
+    }
+}
