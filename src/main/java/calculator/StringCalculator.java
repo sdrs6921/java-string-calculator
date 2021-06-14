cc2ec96b4cb688248c8ce545c8c9d1c43d67a8a2
Jiwoo Kim <jiwoo.kim.dev@gmail.com>
2021-06-15 03:00:48 +0900

    feat: split and sum with basic delimiters

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,10 +1,34 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
 
     public static int splitAndSum(String text) {
         if (isNullOrEmpty(text)) return 0;
-        return 1;
+        String[] tokens = split(text);
+        return sum(tokens);
+    }
+
+    private static int sum(String[] tokens) {
+        int sum = 0;
+        for (String token : tokens) sum += Integer.parseInt(token);
+        return sum;
+    }
+
+    private static String[] split(String text) {
+        List<String> delimiters = Arrays.asList(",", ":");
+        String delimiterPattern = buildDelimiterPattern(delimiters);
+        return text.split(delimiterPattern);
+    }
+
+    private static String buildDelimiterPattern(List<String> delimiters) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (String delimiter : delimiters) sb.append(delimiter);
+        sb.append("]");
+        return sb.toString();
     }
 
     private static boolean isNullOrEmpty(String text) {
