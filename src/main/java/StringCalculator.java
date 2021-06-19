536470c01c53acc1f8548633a6ea8be016db88fe
Chloe <page.prologue@gamil.com>
2021-06-19 09:46:49 +0900

    코드리뷰 반영 - StringUtils, IntStream, 변수명 변경

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,35 +1,38 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    private static final String NUMBER = "^[0-9]*$";
+    private static final String POSITIVE_NUMBER_REX = "^[0-9]*$";
     private static final String DELIMITER = "[,:]";
     private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
 
     public static int splitAndSum(String input) {
-        if (input == null || input.isEmpty()) return 0;
+        if (StringUtils.isEmpty(input)) {
+            return 0;
+        }
+        return sum(split(input));
+    }
 
+    private static String[] split(String input) {
         String[] tokens = input.split(DELIMITER);
         Matcher m = CUSTOM_PATTERN.matcher(input);
         if (m.find()) {
             String customDelimiter = m.group(1);
             tokens = m.group(2).split(customDelimiter);
         }
-
-        return sum(tokens);
+        return tokens;
     }
 
     private static int sum(String[] tokens) {
-        int sum = 0;
-        for (String t : tokens) {
-            sum += parseInt(t);
-        }
-        return sum;
+        return Arrays.stream(tokens).mapToInt(t -> getPositiveNumber(t)).sum();
     }
 
-    private static int parseInt(String token) {
-        if (!Pattern.matches(NUMBER, token)) {
-            throw new RuntimeException("입력값이 잘못되었습니다.");
+    private static int getPositiveNumber(String token) {
+        if (!Pattern.matches(POSITIVE_NUMBER_REX, token)) {
+            throw new RuntimeException("양의 정수 값이 입력되어야 합니다.");
         } 
         return Integer.parseInt(token);
     }
