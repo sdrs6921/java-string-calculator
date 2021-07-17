b26cabfd8a951295a1029c419082b17e77e0d1dd
sdrs6921 <sdrs6921@naver.com>
2021-07-17 18:19:42 +0900

    refactor : null check 시 가독성을 높이기 위해 isNull 메서드로 변경

--- src/main/java/model/domain/Operand.java
+++ src/main/java/model/domain/Operand.java
@@ -1,5 +1,6 @@
 package model.domain;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class Operand {
@@ -9,7 +10,7 @@ public class Operand {
     private static final Pattern OPERAND_PATTERN = Pattern.compile(OPERAND_PATTERN_REGEX);
 
     private Operand(String value) {
-        if (value == null || !OPERAND_PATTERN.matcher(value).matches()) {
+        if (Objects.isNull(value) || !OPERAND_PATTERN.matcher(value).matches()) {
             throw new IllegalArgumentException("올바른 피연산자가 아닙니다");
         }
 
