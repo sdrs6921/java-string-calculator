506f0278def5dd090a9c7abbb9d8f2bb04295c0f
sdrs6921 <sdrs6921@naver.com>
2021-07-25 16:13:39 +0900

    refactor : Operator Functional 메서드 구현

--- src/main/java/model/domain/operator/Operator.java
+++ src/main/java/model/domain/operator/Operator.java
@@ -2,16 +2,18 @@ package model.domain.operator;
 
 import java.util.Arrays;
 
-public enum Operator {
-    PLUS("+"),
-    MINUS("-"),
-    TIMES("*"),
-    DIVIDED_BY("/");
+public enum Operator implements Operation {
+    PLUS("+", Integer::sum),
+    MINUS("-", (x, y) -> x - y),
+    TIMES("*", (x, y) -> x * y),
+    DIVIDED_BY("/", Operator::divide);
 
+    private final Operation operation;
     private final String symbol;
 
-    Operator(String symbol) {
+    Operator(final String symbol, final Operation operation) {
         this.symbol = symbol;
+        this.operation = operation;
     }
 
     public static Operator of(final String symbol) {
@@ -23,5 +25,18 @@ public enum Operator {
 
     public String symbol() {
         return this.symbol;
+    }
+
+    @Override
+    public int operate(int x, int y) {
+        return operation.operate(x, y);
+    }
+
+    private static int divide(int x, int y) {
+        if (y == 0) {
+            throw new IllegalArgumentException("0으로 나눌 수 없습니다");
+        }
+
+        return x / y;
     }
 }
--- src/test/java/model/domain/operator/OperatorTest.java
+++ src/test/java/model/domain/operator/OperatorTest.java
@@ -3,6 +3,7 @@ package model.domain.operator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,10 +25,30 @@ class OperatorTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "string", "한글"})
-    @DisplayName("유효하지 않은 연산자는 예외를 발생시킵니다")
+    @DisplayName("유효하지 않은 연산자는 예외를 발생시킨다")
     void validateOperator(String invalidOperator) {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> Operator.of(invalidOperator))
                 .withMessage("유효한 연산자가 아닙니다");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"3,1", "4,1", "5,2"})
+    @DisplayName("두 개의 숫자를 입력 받아 연산을 수행한다")
+    void operate(int x, int y) {
+        assertAll(
+                () -> assertThat(Operator.PLUS.operate(x, y)).isEqualTo(x + y),
+                () -> assertThat(Operator.MINUS.operate(x, y)).isEqualTo(x - y),
+                () -> assertThat(Operator.TIMES.operate(x, y)).isEqualTo(x * y),
+                () -> assertThat(Operator.DIVIDED_BY.operate(x, y)).isEqualTo(x / y)
+        );
+    }
+
+    @Test
+    @DisplayName("0으로 나누게 되면 예외를 발생시킨다")
+    void operate_throw_exception_with_divided_by_zero() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Operator.DIVIDED_BY.operate(3, 0))
+                .withMessage("0으로 나눌 수 없습니다");
     }
 }
