8240708bd08099d6d058e70583c585de4a3f2325
sdrs6921 <sdrs6921@naver.com>
2021-08-01 15:11:23 +0900

    refactor : Calculator 수식을 받아서 계산하는 메서드 구현

--- src/main/java/model/domain/Calculator.java
+++ src/main/java/model/domain/Calculator.java
@@ -1,57 +1,15 @@
 package model.domain;
 
-import model.domain.operator.Operator;
-
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class Calculator {
-    private final Expression expression;
-    private int answer;
 
-    public Calculator(final Expression expression) {
-        this.expression = expression;
-        answer = expression.getOperands().getFirstElement();
+    public Calculator() {
     }
 
-    public int calculate() {
-        AtomicInteger index = new AtomicInteger();
-
-        expression.getOperators()
-                .stream()
-                .forEach(operator -> parseOperator(operator, expression.getOperands().getElement(index.incrementAndGet())));
-
-        return answer;
-    }
-
-    private void parseOperator(Operator operator, int operand) {
-        if (operator.equals(Operator.PLUS)) {
-            add(operand);
-        } else if (operator.equals(Operator.MINUS)) {
-            subtract(operand);
-        } else if (operator.equals(Operator.TIMES)) {
-            multiply(operand);
-        } else if (operator.equals(Operator.DIVIDED_BY)) {
-            divide(operand);
-        }
-    }
-
-    private void add(final int operand) {
-        this.answer += operand;
-    }
-
-    private void subtract(final int operand) {
-        this.answer -= operand;
-    }
-
-    private void multiply(final int operand) {
-        this.answer *= operand;
-    }
-
-    private void divide(final int operand) {
-        if (operand == 0) {
-            throw new IllegalArgumentException("0으로 나눌 수 없습니다");
-        }
-
-        this.answer /= operand;
+    public int calculate(final Expression expression) {
+        return IntStream.range(0, expression.operatorSize())
+                .reduce(expression.firstOperand(),
+                        (total, index) -> total = expression.operatorIndexOf(index).operate(total, expression.operandIndexOf(index + 1)));
     }
 }
--- src/test/java/model/domain/CalculatorTest.java
+++ src/test/java/model/domain/CalculatorTest.java
@@ -12,15 +12,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CalculatorTest {
+    private final Calculator calculator = new Calculator();
 
     @ParameterizedTest
     @MethodSource("validParameterProvider")
     @DisplayName("Expression을 입력받아 값을 계산하여 반환한다")
-    void calculate(String[] token, int answer) {
+    void calculate2(String[] token, int answer) {
         Expression expression = new Expression(token);
-        Calculator calculator = new Calculator(expression);
 
-        int expect = calculator.calculate();
+        int expect = calculator.calculate(expression);
 
         assertThat(expect).isEqualTo(answer);
     }
@@ -30,16 +30,16 @@ class CalculatorTest {
     void calculate_throw_exception_with_divide_by_zero() {
         String[] invalidToken = {"1", "/", "0"};
         Expression expression = new Expression(invalidToken);
-        Calculator calculator = new Calculator(expression);
 
         assertThatIllegalArgumentException()
-                .isThrownBy(calculator::calculate)
+                .isThrownBy(() -> calculator.calculate(expression))
                 .withMessage("0으로 나눌 수 없습니다");
     }
 
     static Stream<Arguments> validParameterProvider() {
         return Stream.of(
                 Arguments.of(new String[]{"4", "+", "5"}, 9), Arguments.of(new String[]{"2", "-", "1"}, 1),
-                Arguments.of(new String[]{"1", "*", "2"}, 2), Arguments.of(new String[]{"2", "/", "1"}, 2));
+                Arguments.of(new String[]{"1", "*", "2"}, 2), Arguments.of(new String[]{"2", "/", "1"}, 2),
+                Arguments.of(new String[]{"4", "+", "1", "-", "2"}, 3));
     }
 }
